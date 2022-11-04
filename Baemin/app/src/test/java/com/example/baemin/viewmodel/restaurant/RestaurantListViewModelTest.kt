65766a37828d71312eb7a9a2aa35ce7ae1888ff9
jungmin <kimjm0825@gmail.com>
2022-11-05 02:00:21 +0900

    테스트 코드 작성

--- Baemin/app/src/test/java/com/example/baemin/viewmodel/restaurant/RestaurantListViewModelTest.kt
+++ Baemin/app/src/test/java/com/example/baemin/viewmodel/restaurant/RestaurantListViewModelTest.kt
@@ -5,10 +5,13 @@ import com.example.baemin.data.repository.restaurant.RestaurantRepository
 import com.example.baemin.model.restaurant.RestaurantModel
 import com.example.baemin.screen.main.home.restaurant.RestaurantCategory
 import com.example.baemin.screen.main.home.restaurant.RestaurantListViewModel
+import com.example.baemin.screen.main.home.restaurant.RestaurantOrder
 import com.example.baemin.viewmodel.ViewModelTest
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.test.runBlockingTest
 import org.junit.Test
+
 import org.koin.core.parameter.parametersOf
 import org.koin.test.inject
 
@@ -32,7 +35,7 @@ internal class RestaurantListViewModelTest: ViewModelTest() {
     private val restaurantRepository by inject<RestaurantRepository>()
 
     @Test
-    fun `test load restaurant list category All`() = runBlocking {
+    fun `test load restaurant list category All`() = runBlocking<Unit> {
         val testObservable = restaurantListViewModel.restaurantListLiveData.test()
 
         restaurantListViewModel.fetchData()
@@ -53,6 +56,49 @@ internal class RestaurantListViewModelTest: ViewModelTest() {
                         restaurantTelNumber = it.restaurantTelNumber
                     )
                 }
+            )
+        )
+    }
+
+    @Test
+    fun `test load restaurant list category Excepted`() = runBlockingTest {
+        restaurantCategory = RestaurantCategory.CAFE_DESSERT
+
+        val testObservable = restaurantListViewModel.restaurantListLiveData.test()
+
+        restaurantListViewModel.fetchData()
+
+        testObservable.assertValuesSequence(
+            listOf(
+                listOf()
+            )
+        )
+    }
+
+    @Test
+    fun `test load restaurant list order by fast delivery time`() = runBlockingTest {
+        val testObservable = restaurantListViewModel.restaurantListLiveData.test()
+
+        restaurantListViewModel.setRestaurantOrder(RestaurantOrder.FAST_DELIVERY)
+
+        testObservable.assertValuesSequence(
+            listOf(
+                restaurantRepository.getList(restaurantCategory, locationLatLngEntity)
+                    .sortedBy { it.deliveryTimeRange.first }
+                    .map {
+                        RestaurantModel(
+                            id = it.id,
+                            restaurantInfoId = it.restaurantInfoId,
+                            restaurantCategory = it.restaurantCategory,
+                            restaurantTitle = it.restaurantTitle,
+                            restaurantImageUrl = it.restaurantImageUrl,
+                            grade = it.grade,
+                            reviewCount = it.reviewCount,
+                            deliveryTimeRange = it.deliveryTimeRange,
+                            deliveryTipRange = it.deliveryTipRange,
+                            restaurantTelNumber = it.restaurantTelNumber
+                        )
+                    }
             )
         )
     }
